d3bc64abf19ed0012fb3172e7f2768910ced971b
LeonAdeoye <leon.adeoye@gmail.com>
2025-05-03 23:27:01 +0800

    added timestamp to order

--- generator/src/main/java/com/trading/model/Order.java
+++ generator/src/main/java/com/trading/model/Order.java
@@ -1,10 +1,5 @@
 package com.trading.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import java.util.Currency;
 import java.util.UUID;
@@ -17,8 +12,5 @@ public record Order(
         double price,
         TradeSide side,
         Currency currency,
-//        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-//        @JsonSerialize(using = LocalDateTimeSerializer.class)
-//        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
         LocalDateTime tradeTimestamp
 ) {}
